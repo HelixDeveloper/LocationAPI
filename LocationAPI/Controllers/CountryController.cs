9e79db57af3a056cccf06df1a6077af844b764a0
bhadresh gosai <126605450+bhadresh1989@users.noreply.github.com>
2024-01-18 11:42:28 -0500

    Add project files.

---
+++ LocationAPI/Controllers/CountryController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using LocationAPI.Models;
+
+namespace LocationAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        List<CountryModel> countries = new List<CountryModel>{
+
+                new CountryModel(){
+                CountryId = 1,
+                CountryName = "India"
+                },
+                new CountryModel(){
+                CountryId = 2,
+                CountryName = "USA"
+                },
+                new CountryModel(){
+                CountryId = 3,
+                CountryName = "Canada"
+                },
+                new CountryModel(){
+                CountryId = 4,
+                CountryName = "UK"
+                },
+            };
+
+
+        [HttpGet(Name = "GetCountries")]
+        public ActionResult<List<CountryModel>> Get()
+        {
+
+            return Ok(countries);
+        }
+        [HttpGet("{id}")]
+        public ActionResult<CountryModel> Get(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest("Id should not be zero or negative");
+            }
+
+            return Ok(countries.Where(a => a.CountryId == id));
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] VMCountry country)
+        {
+            try
+            {
+
+                if (country == null)
+                {
+                    return BadRequest("Please add valid data!");
+                }
+                countries.Add(new CountryModel() { 
+                    CountryId = 0,
+                    CountryCode = country.CountryCode,
+                    CountryName = country.CountryName,
+                });
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ LocationAPI/Models/CountryModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LocationAPI.Models
+{
+    public class CountryModel
+    {
+        [Key]
+        public int CountryId { get; set; }
+        [Required]
+        public string CountryName { get; set; } = string.Empty;
+        [Required]
+        public string CountryCode { get; set; }
+    }
+    public class VMCountry
+    {
+        [Required]
+        public string CountryName { get; set; } = string.Empty;
+        [Required]
+        public string CountryCode { get; set; }
+    }
+}
